5f1852bde5f9b665d6ab690cd53f622e653e56c0
rnjswltn1020 <rnjswltn1020@naver.com>
2023-05-16 10:31:16 +0900

    **master-commit
    1.prettier 설정
    2.메인 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 //common
-
-import { Route, Router, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Main from './pages/main/Main';
 import Footer from './pages/common/Footer';
 import Header from './pages/common/Header';
@@ -8,12 +7,12 @@ import './scss/style.scss';
 
 export default function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Main}></Route>
         <Route path="/" render={() => <Header type="sub" />} />
       </Switch>
       <Footer />
-    </Router>
+    </BrowserRouter>
   );
 }
